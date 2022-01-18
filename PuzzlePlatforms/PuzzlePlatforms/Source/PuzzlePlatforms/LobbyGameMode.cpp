0bcefbd724172c249cf71826fc2b7643a212645d
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2022-01-19 00:05:28 +0100

    starting game with delay on min players

--- PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -3,22 +3,22 @@
 
 #include "LobbyGameMode.h"
 
+#include "TimerManager.h"
+
+const static int32 MIN_PLAYERS = 2;
+const static float START_DELAY = 5;
+
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
 	++NumberOfPlayers;
 
-	if (NumberOfPlayers >= 3)
+	if (NumberOfPlayers >= MIN_PLAYERS)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Reached 3 players!"));
+		UE_LOG(LogTemp, Warning, TEXT("Reached %d players!"), MIN_PLAYERS);
 
-
-		UWorld* World = GetWorld();
-		if (!ensure(World != nullptr)) return;
-
-		bUseSeamlessTravel = true;
-		World->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
+		GetWorldTimerManager().SetTimer(GameStartTimer, this, &ALobbyGameMode::StartGame, START_DELAY);
 	}
 }
 void ALobbyGameMode::Logout(AController* Exiting)
@@ -26,4 +26,13 @@ void ALobbyGameMode::Logout(AController* Exiting)
 	Super::Logout(Exiting);
 
 	--NumberOfPlayers;
+}
+
+void ALobbyGameMode::StartGame()
+{
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	bUseSeamlessTravel = true;
+	World->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
 }
--- PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.h
+++ PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.h
@@ -20,4 +20,7 @@ public:
 
 private:
 	uint32 NumberOfPlayers = 0;
+	FTimerHandle GameStartTimer;
+
+	void StartGame();
 };
