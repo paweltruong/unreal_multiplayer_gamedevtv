6b88166a39e82751617508eacf25f5af9247f8d2
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2022-01-18 23:15:23 +0100

    fixed pawn spawning bug

--- PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ PuzzlePlatforms/PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -5,6 +5,8 @@
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
+	Super::PostLogin(NewPlayer);
+
 	++NumberOfPlayers;
 
 	if (NumberOfPlayers >= 3)
@@ -14,5 +16,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 }
 void ALobbyGameMode::Logout(AController* Exiting)
 {
+	Super::Logout(Exiting);
+
 	--NumberOfPlayers;
 }
